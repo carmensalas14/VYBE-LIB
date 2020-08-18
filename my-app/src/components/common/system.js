d099c686add1beb0b8f245edae22747e6bb625c7
Carmen Salas <carmensalas0114@gmail.com>
2020-08-18 13:33:08 -0400

    added objects defining heights and widths for button, can be reused for other block elements

---
+++ my-app/src/components/common/system.js
@@ -0,0 +1,27 @@
+// size for button heights
+export const blockSizeHeights = {
+  sm: '45px',
+  md: '130px',
+  lg: '110px',
+};
+
+// sizes for button widths
+export const blockSizeWidths = {
+  sm: '10%',
+  md: '270px',
+  lg: '250px',
+};
+
+// sizes for button scaling
+export const scaledSizes = {
+  sm: 'scale(0.9)',
+  md: 'scale(0.6)',
+  lg: 'scale(0.9)',
+};
+
+// sizes for fonts
+export const fontSizes = {
+  sm: '16px',
+  md: '17px',
+  lg: '27px',
+};
